72a9316cccc68c328ee697c9da48ec155ff96236
vadymsem <vadym.sem18@gmail.com>
2018-02-18 15:37:35 +0200

    fixick

---
+++ main.js
@@ -0,0 +1,5 @@
+var imported = document.createElement('script');
+var imported2 = document.createElement('script');
+imported.src = './bower_components/jquery/dist/jquery.min.js';
+imported2.src = './bower_components/bootstrap/dist/js/bootstrap.min.js';
+document.head.appendChild(imported);
